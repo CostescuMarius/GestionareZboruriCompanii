417b994ce5a5f438d707d35df9a460142a8803d6
Constantin-Marius Costescu <101038739+CostescuMarius@users.noreply.github.com>
2023-03-01 22:37:33 +0200

    01.03.2023 - c4

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -2,6 +2,8 @@ package Model;
 
 import View.ApplicationWindow;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
         ApplicationWindow Application = new ApplicationWindow();
